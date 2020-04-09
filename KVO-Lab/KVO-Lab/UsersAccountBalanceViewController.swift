14896dc77a7357d54d349ed41d3064e9ec5688af
tanyaburke <tanyaburke@pursuit.org>
2020-04-09 11:50:27 -0400

    Added UI

---
+++ KVO-Lab/KVO-Lab/UsersAccountBalanceViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UsersAccountBalanceViewController.swift
+//  KVO-Lab
+//
+//  Created by Tanya Burke on 4/9/20.
+//  Copyright © 2020 Tanya Burke. All rights reserved.
+//
+
+import UIKit
+
+class UsersAccountBalanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
